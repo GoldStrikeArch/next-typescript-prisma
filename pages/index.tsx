50a4085fa708df4bfc4641650fe5edcb28483f58
Mikhail Pertsev <arthasmontano@mail.ru>
2021-12-27 19:33:37 +0300

    basic UI structure for '/' page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,16 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <main className="h-screen w-screen flex flex-col justify-center items-center">
+      <h1 className="text-2xl text-center">Which pokemon is rounder?</h1>
+      <section className="border rounded p-8 mt-4 flex justify-between max-w-2xl items-center">
+        <div className="bg-red-400 w-16 h-16">ASd</div>
+        <div>VS</div>
+        <div className="bg-red-400 w-16 h-16">ASd</div>
+      </section>
+    </main>
+  );
 };
 
 export default Home;
